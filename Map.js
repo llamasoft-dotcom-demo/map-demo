c0b3a1775db1ea074e81ba4e1c56e9c1b70b4ca1
Anton Suarez <suarezar@gmail.com>
2016-02-09 11:13:56 -0800

    add a selected list of airports, refactor js so that clicking the list items is the same as changing the select menu

--- Map.js
+++ Map.js
@@ -20,47 +20,79 @@ var MapFcns = {
     },
     
     siteListChange: function() {
-        var ctl = $(this),
-            airportCode = ctl.val();
-            if(airportCode) {
-                var currAirport = _.findWhere(sites, {Code: airportCode});
-                $('#setting-code').text(currAirport.Code);
-                $('#setting-city').text(currAirport.City);
-                $('#setting-state').text(currAirport.State);
-                $('#setting-lat').text(currAirport.Latitude);
-                $('#setting-long').text(currAirport.Longitude);
+        var ctl = $(this);
+        airportCode = ctl.val();
+        if (airportCode) {
+            MapFcns.doSiteChange(airportCode);
+        }
+    },
 
-                // this one is rather large...and since I'm moving the whole table to right of the map..protect against overflow
-                // also I'm peeling off the AIRPORT_CODE_X part and just making X...the rest seems like its just taking up space for no reason
-                var useName = currAirport.FullSiteName.split('_')[2];
-                var fullNameElement = document.createElement('p');
-                fullNameElement.innerHTML = useName;
-                $('#setting-fullname').empty();
-                $('#setting-fullname').append(fullNameElement);
+    doSiteChange: function(airportCode){
+        var currAirport = _.findWhere(sites, {Code: airportCode});
+        $('#setting-code').text(currAirport.Code);
+        $('#setting-city').text(currAirport.City);
+        $('#setting-state').text(currAirport.State);
+        $('#setting-lat').text(currAirport.Latitude);
+        $('#setting-long').text(currAirport.Longitude);
 
-                // only add a marker if its not already there
-                if (MapFcns.markerVault.indexOf(currAirport.Code) == -1){
-                    var pos = {lat: currAirport.Latitude, lng: currAirport.Longitude};
-                    var marker = new google.maps.Marker({
-                        position: pos,
-                        map: globalMap,
-                        title: currAirport.Code
-                    });
+        // this one is rather large...and since I'm moving the whole table to right of the map..protect against overflow
+        // also I'm peeling off the AIRPORT_CODE_X part and just making X...the rest seems like its just taking up space for no reason
+        var useName = currAirport.FullSiteName.split('_')[2];
+        var fullNameElement = document.createElement('p');
+        fullNameElement.innerHTML = useName;
+        $('#setting-fullname').empty();
+        $('#setting-fullname').append(fullNameElement);
 
-                    // add a listener to the click event here...for now, just make this remove the marker
-                    marker.addListener('click', function(){
-                        marker.setMap(null);
-                        // remove from the vault as well
-                        MapFcns.markerVault.splice(MapFcns.markerVault.indexOf(currAirport.Code), 1);
-                    });
+        var pos = {lat: currAirport.Latitude, lng: currAirport.Longitude};
 
-                    // add to the marker vault to prevent dups 
-                    MapFcns.markerVault.push(currAirport.Code);
+        // only add a marker if its not already there
+        if (MapFcns.markerVault.indexOf(currAirport.Code) == -1){
+            var marker = new google.maps.Marker({
+                position: pos,
+                map: globalMap,
+                title: currAirport.Code
+            });
 
-                    // center the map to what just got added
-                    globalMap.setCenter(pos);
-                }
-            }
+            // add a listener to the click event here...for now, just make this remove the marker
+            marker.addListener('click', function(){
+                marker.setMap(null);
+                // remove from the vault as well
+                MapFcns.removeVault(currAirport.Code, 1);
+            });
+
+            // add to the marker vault to prevent dups 
+            MapFcns.addVault(currAirport.Code);
+        }
+
+        // center the map to what just got clicked
+        globalMap.setCenter(pos);
+
+        // since there are muliple ways to get in here now, make sure the airport code is selected in the drop down
+        var airportList = $('#airport-list').val(currAirport.Code);
+    },
+
+    addVault: function(code){
+        this.markerVault.push(code);
+        this.updateSelectedList();
+    },
+
+    removeVault: function(code){
+        this.markerVault.splice(this.markerVault.indexOf(code), 1);
+        this.updateSelectedList();
+    },
+
+    updateSelectedList: function(){
+        // for ease..just recreate the list when this is called
+        var sortedVault = _.sortBy(this.markerVault, function(i){ return i.toLowerCase(); });
+        
+        $('#airport-recents-list').empty();
+        _.each(sortedVault, function(i){
+            var newListElement = document.createElement('li');
+            newListElement.innerHTML = i;
+            // add a listener that will go into the siteListChange function
+            $(newListElement).click(function(){ MapFcns.doSiteChange(i) });
+            $('#airport-recents-list').append(newListElement);
+        });
     }
 }
 
