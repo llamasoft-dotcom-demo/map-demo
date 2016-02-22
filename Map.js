0baf4774fc6ce7c77d5c318622e9291fcb460fe7
Kyle <nylan1kc@cmich.edu>
2016-02-22 08:25:16 -0500

    Updated map
    -Added map to the page by filling in a google api key
    -Filled in missing data for each airport
    -Sorted airports alphabetically
    -Updated CSS to look better/ use less space
    -Added button to toggle markers

--- Map.js
+++ Map.js
@@ -1,16 +1,28 @@
 var globalMap;
+var markers =[];
 
 $(function() {
 
 var MapFcns = {
     loadSiteList: function () {
         var airportList = $('#airport-list');
+		var airportChildren = $('#airport-list')
             airportList.html('');
             airportList.append('<option value=""></option>');
         for (var i in sites) {
             var newOption = $('<option value="' + sites[i].Code + '">' + sites[i].Code + '</option>');
             airportList.append(newOption);
         }
+		var optionList = $('#airport-list option');
+		optionList.sort(function(a, b){
+			if (a.text > b.text)
+				return 1;
+			else if (a.text < b.text)
+				return -1;
+			else
+				return 0;
+		});
+		$("#airport-list").empty().append(optionList);
     },
     
     siteListChange: function() {
@@ -20,12 +32,20 @@ var MapFcns = {
                 var currAirport = _.findWhere(sites, {Code: airportCode});
                 $('#setting-code').text(currAirport.Code);
                 $('#setting-city').text(currAirport.City);
+				$('#setting-state').text(currAirport.State);
+				var fullsiteName = currAirport.FullSiteName.split("_");
+				$('#setting-fullname').text(fullsiteName[2]);
+				$('#setting-lat').text(currAirport.Latitude);
+				$('#setting-long').text(currAirport.Longitude);
                 
+				globalMap.panTo({lat: currAirport.Latitude, lng: currAirport.Longitude});
+				
                 var marker = new google.maps.Marker({
                     position: {lat: currAirport.Latitude, lng: currAirport.Longitude},
                     map: globalMap,
                     title: currAirport.Code
                 });
+				markers.push(marker);
             }
     }
 }
@@ -47,9 +67,33 @@ $('#exercise-toggle').click(function() {
 
 });
 
-
-
-
+$(function() {
+   $('#toggleBtn').click(function() {
+	   var airportCode = $('#airport-list').val();
+	   var check = false;
+	   if (airportCode)
+	   {
+		   var currAirport = _.findWhere(sites, {Code: airportCode});
+		   for (var i in markers){
+			   if (currAirport.Code == markers[i].title)
+			   {
+				   markers[i].setMap(null);
+				   markers.splice(i, 1);
+				   check = true;
+			   }
+		   }
+		   if (check == false)
+		   {
+				var marker = new google.maps.Marker({
+                    position: {lat: currAirport.Latitude, lng: currAirport.Longitude},
+                    map: globalMap,
+                    title: currAirport.Code
+                });
+				markers.push(marker);
+		   }
+	   }
+   });
+});
 
 
 
