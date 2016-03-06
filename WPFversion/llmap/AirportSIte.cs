6fb0a0d5ce782700379cd7f91e994c8fb2a4d6d3
Jeff Spindler <jeff@perform3-d.com>
2016-03-06 10:53:52 -0500

    WPF version

---
+++ WPFversion/llmap/AirportSIte.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace llmap
+{
+    class AirportSite
+    {
+        public string Code { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string FullSiteName { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public string LatLong
+        {
+            get
+            {
+                return Latitude.ToString() + ", " + Longitude.ToString();
+            }
+        }
+    }
+}
+
