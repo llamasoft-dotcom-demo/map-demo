61420d94d8a85883bcaaf4c8212914bafb61bc53
pekj <pekj@umich.edu>
2016-04-04 21:37:43 -0400

    display all airport data, sort airport array, click to remove marker

--- Map.js
+++ Map.js
@@ -1,33 +1,52 @@
 var globalMap;
 
+function sortByCode(lhs, rhs) {
+    if (lhs.Code < rhs.Code)
+        return -1;
+    else if (lhs.Code > rhs.Code)
+        return 1;
+    else
+        return 0;
+}
+
 $(function() {
 
-var MapFcns = {
-    loadSiteList: function () {
-        var airportList = $('#airport-list');
-            airportList.html('');
-            airportList.append('<option value=""></option>');
-        for (var i in sites) {
-            var newOption = $('<option value="' + sites[i].Code + '">' + sites[i].Code + '</option>');
-            airportList.append(newOption);
-        }
-    },
-    
-    siteListChange: function() {
-        var ctl = $(this),
-            airportCode = ctl.val();
-            if(airportCode) {
-                var currAirport = _.findWhere(sites, {Code: airportCode});
-                $('#setting-code').text(currAirport.Code);
-                $('#setting-city').text(currAirport.City);
-                
-                var marker = new google.maps.Marker({
-                    position: {lat: currAirport.Latitude, lng: currAirport.Longitude},
-                    map: globalMap,
-                    title: currAirport.Code
-                });
+    var MapFcns = {
+        loadSiteList: function () {
+            var airportList = $('#airport-list');
+                airportList.html('');
+                airportList.append('<option value=""></option>');
+
+            sites.sort(sortByCode);
+            for (var i in sites) {
+                var newOption = $('<option value="' + sites[i].Code + '">' + sites[i].Code + '</option>');
+                airportList.append(newOption);
             }
-    }
+        },
+        
+        siteListChange: function() {
+            var ctl = $(this),
+                airportCode = ctl.val();
+                if(airportCode) {
+                    var currAirport = _.findWhere(sites, {Code: airportCode});
+                    $('#setting-code').text(currAirport.Code);
+                    $('#setting-city').text(currAirport.City);
+                    $('#setting-state').text(currAirport.State);
+                    $('#setting-fullName').text(currAirport.FullSiteName);
+                    $('#setting-lat').text(currAirport.Latitude);
+                    $('#setting-long').text(currAirport.Longitude);
+                    
+                    var marker = new google.maps.Marker({
+                        position: {lat: currAirport.Latitude, lng: currAirport.Longitude},
+                        map: globalMap,
+                        title: currAirport.Code
+                    });
+
+                    google.maps.event.addListener(marker, "click", function() {
+                        marker.setMap(null);
+                    });
+                }
+        }
 }
 
 
