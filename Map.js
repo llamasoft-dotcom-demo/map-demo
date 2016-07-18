e43b33766c22b7168df25b720d48fd7e38450572
Cara Canady <cara.canady@gmail.com>
2016-07-18 01:58:55 -0400

    fixes include updated user instructions, duplicate marker fix

--- Map.js
+++ Map.js
@@ -1,18 +1,20 @@
 var globalMap;
 
 $(function () {
-
+  
 //=================================================
 //this fcn populates the dropdown from siteData.js
 //created new array that's sorted and populated dropdown
 //from that new array.
 //=================================================
+
+
    var MapFcns = {
       loadSiteList: function () {
       var airportList = $('#airport-list');
           airportList.html('');
           airportList.append('<option value=""></option>');
-      var array = [];
+          var array = [];
       for (var i in sites) {
               array.push(sites[i].City);
               array.sort();
@@ -24,7 +26,7 @@ $(function () {
           
   },
 //=================================================
-//diplays airport information on table and creates marker.
+//diplays airport information on table 
 //=================================================
       siteListChange: function() {
            var ctl = $(this),
@@ -34,10 +36,13 @@ $(function () {
                $('#setting-code').text(currAirport.Code);
                $('#setting-city').text(currAirport.City);
                $('#setting-state').text(currAirport.State);
-               $('#setting-fullname').text(currAirport.FullSiteName);
+               $('#setting-fullname').text(currAirport.FullSiteName.slice(12));
                $('#setting-lat').text(currAirport.Latitude);
                $('#setting-long').text(currAirport.Longitude);
                              
+//=================================================
+//places a unique marker on the map.
+//=================================================        
                var id;
                var markers = {};
                var marker = new google.maps.Marker({
@@ -47,19 +52,25 @@ $(function () {
                });
 
 //=================================================
+//solves duplicate marker issue.
+//=================================================  
+                $("#airport-list :selected").remove();
+
+//=================================================
+//function for the user to delete the marker
+//=================================================
+               var delMarker = function (id) {
+                   marker = markers[id];
+                   marker.setMap(null);
+               };
+
+//=================================================
 //pans to the location of the marker.
 //=================================================
                globalMap.panTo({lat: currAirport.Latitude, lng: currAirport.Longitude});
                id = marker._gm_id;
                markers[id] = marker;
                    google.maps.event.addListener(marker, "dblclick", function (point) { id = this.__gm_id; delMarker(id) });
-
-//===============================================           //function for the user to delete the marker
-//=================================================
-               var delMarker = function (id) {
-                   marker = markers[id];
-                   marker.setMap(null);
-               };
 
 //=================================================
 //creates infowindow for better UX & conserves screen area.
@@ -76,6 +87,7 @@ $(function () {
                 });
  
                 marker.addListener('click', function() {
+                  
                     infowindow.open(globalMap, marker);
                 });
                
@@ -92,18 +104,19 @@ $(function () {
    $('#exercise-toggle').click(function() {
        var  toggleCtl = $(this),
            toggleVal = toggleCtl.text();
-       if (toggleVal == '-') {
-           toggleCtl.text('+');
+       if (toggleVal == 'Click to condense instructions') {
+           toggleCtl.text('Click to show instructions');
            $('#exercise-instructions').hide();
        } else {
-           toggleCtl.text('-');
+           toggleCtl.text('Click to condense instructions');
            $('#exercise-instructions').show();
        }
    });
 
 });
- //=================================================
-// Callback function to create a map object and specify the DOM element for display.
+//=================================================
+// Callback function to create a map object and specify the DOM element for 
+//display.
 //================================================= 
 function  initMap() {
 
