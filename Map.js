db8756a0aefe10d9f2fdc598225b8af5ce0e1278
Ray Walters <rlwalters3@yahoo.com>
2016-07-30 20:29:16 -0400

    Initial commit

--- Map.js
+++ Map.js
@@ -1,4 +1,5 @@
 var globalMap;
+var markers = [];
 
 $(function() {
 
@@ -7,11 +8,16 @@ var MapFcns = {
         var airportList = $('#airport-list');
             airportList.html('');
             airportList.append('<option value=""></option>');
+            sites = _.sortBy(sites,'Code');//sort sites array by Code
         for (var i in sites) {
             var newOption = $('<option value="' + sites[i].Code + '">' + sites[i].Code + '</option>');
             airportList.append(newOption);
         }
+
     },
+
+ 
+
     
     siteListChange: function() {
         var ctl = $(this),
@@ -20,6 +26,17 @@ var MapFcns = {
                 var currAirport = _.findWhere(sites, {Code: airportCode});
                 $('#setting-code').text(currAirport.Code);
                 $('#setting-city').text(currAirport.City);
+                $('#setting-state').text(currAirport.State);
+                $('#setting-full-name').text(currAirport.FullSiteName);//id of setting name was used twice
+                $('#setting-lat').text(currAirport.Latitude);
+                $('#setting-long').text(currAirport.Longitude);
+
+// *** Center map based off of drop down
+                globalMap = new google.maps.Map(document.getElementById('airport-map'), {
+                    center: {lat: currAirport.Latitude, lng: currAirport.Longitude},
+                    scrollwheel: true,
+                    zoom: 6
+                  });
                 
                 var marker = new google.maps.Marker({
                     position: {lat: currAirport.Latitude, lng: currAirport.Longitude},
@@ -27,7 +44,12 @@ var MapFcns = {
                     title: currAirport.Code
                 });
             }
-    }
+            markers.push(marker);//store marker in array
+
+        
+    },
+
+  
 }
 
 
@@ -49,9 +71,19 @@ $('#exercise-toggle').click(function() {
 
 
 
-
-
-
+// remove markers in array from map view
+  function removeMarkers(){
+        for(i=0; i<markers.length; i++){
+            markers[i].setMap(null);
+        }
+        $('#setting-code').text("");
+        $('#setting-city').text("");
+        $('#setting-state').text("");
+        $('#setting-full-name').text("");
+        $('#setting-lat').text("");
+        $('#setting-long').text("");
+        initMap();
+    }
 
     
 function  initMap() {
@@ -62,4 +94,5 @@ function  initMap() {
     zoom: 6
   });
   
-    }
+}
+
