556530e470f3bf972c143a7fc84765feb725b2df
heerens <alexander.heerens@gmail.com>
2016-08-14 21:36:00 +0200

    select marker on click

--- js/Map.js
+++ js/Map.js
@@ -1,48 +1,3 @@
-var globalMap;
-
-$(function() {
-
-var MapFcns = {
-    
-    
-    siteListChange: function() {
-        var ctl = $(this),
-            airportCode = ctl.val();
-            if(airportCode) {
-                var currAirport = _.findWhere(sites, {Code: airportCode});
-                $('#setting-code').text(currAirport.Code);
-                $('#setting-city').text(currAirport.City);
-                
-                var marker = new google.maps.Marker({
-                    position: {lat: currAirport.Latitude, lng: currAirport.Longitude},
-                    map: globalMap,
-                    title: currAirport.Code
-                });
-            }
-    }
-}
-
-
-$('#airport-list').change(MapFcns.siteListChange);
-$('#exercise-toggle').click(function() {
-    var  toggleCtl = $(this),
-         toggleVal = toggleCtl.text();
-    if (toggleVal == '-') {
-        toggleCtl.text('+');
-        $('#exercise-instructions').hide();
-    } else {
-        toggleCtl.text('-');
-        $('#exercise-instructions').show();
-    }
-});
-
-});
-
-
-
-
-
-
 
     
 function  initMap() {
@@ -53,4 +8,6 @@ function  initMap() {
     zoom: 6
   });
   
-    }
+  angular.element('body').scope().controller.initGoogleMap(globalMap);
+  
+}
--- js/mapController.js
+++ js/mapController.js
@@ -1,18 +1,38 @@
 angular.module('mapApp', []).controller('MapController', function($scope, $http, $filter, $window) {
 	var controller = this;
 
-
 	controller.init = function() {
+	    controller.seletedAirports = [];
 		controller.airports = $window.init.sites;
 		controller.options = _.map(controller.airports, function(airport){ 
 		      return { name : airport.Code, value : airport}; 
 		    });
-		controller.airport = controller.airports[0];
+		controller.airport = null;
 	};
 
 	controller.airportSelected = function() {
-		alert(controller.airport.Code);
+	    var latLng = new google.maps.LatLng(controller.airport.Latitude, controller.airport.Longitude);
+	
+	    if (_.findWhere(controller.seletedAirports, controller.airport) == null) {
+	        var marker = new google.maps.Marker({
+	            position: latLng,
+	            map: controller.map,
+	            title: controller.airport.Code
+	        });	 
+	        
+	        marker.addListener('click', function() {
+	            controller.airport = _.findWhere(controller.airports, {Code: this.title});
+	            // update the UI
+	            $scope.$apply();
+	        });
+	        controller.seletedAirports.push(controller.airport);
+        }
+        controller.map.panTo(latLng);
 	};
+	
+   controller.initGoogleMap = function(map) {
+       controller.map = map;
+    };
 	
 	controller.init();
 });
