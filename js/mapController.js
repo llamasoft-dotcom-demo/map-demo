3fdac0d3f3cd45abb827ec74fbdd1a2d7d187ffd
heerens <alexander.heerens@gmail.com>
2016-08-15 19:59:54 +0200

    added comments

--- js/mapController.js
+++ js/mapController.js
@@ -1,26 +1,39 @@
 angular.module('mapApp', []).controller('MapController', function($scope, $http, $filter, $window) {
 	var controller = this;
 
+	/**
+	 * Method to initialize all fields
+	 */
 	controller.init = function() {
+	    // holds the airport (codes) of the displayed airports
 	    controller.seletedAirports = [];
-	    // get the sorted list
+	    
+	    // list of all airports (sorted by code)
 		controller.airports = _.sortBy($window.init.sites, 'Code');
 		// clean up the names
 		controller.airports = _.map(controller.airports, function(airport){ 
+		    // remove the upper case part
 		    var niceName = airport.FullSiteName.replace("AIRPORT_"+airport.Code+"_", "");
 		    airport.FullSiteName = niceName;
             return airport; 
           });		
+		
+		// create a key/value list used for the dropdown
 		controller.options = _.map(controller.airports, function(airport){ 
 		      return { name : airport.Code, value : airport}; 
 		    });
+		
+		// holds the selected airport
 		controller.airport = null;
 	};
 
+	/**
+	 * Is called once the user selects an airport.
+	 */
 	controller.airportSelected = function() {
 	    var latLng = new google.maps.LatLng(controller.airport.Latitude, controller.airport.Longitude);
 	
-	    
+	    // don't add a marker twice
 	    if (!_.contains(controller.seletedAirports, controller.airport.Code)) {
 	        
 	        var marker = new google.maps.Marker({
@@ -32,23 +45,35 @@ angular.module('mapApp', []).controller('MapController', function($scope, $http,
 	            title: controller.airport.Code
 	        });	 
 	        
+	        // change the selected airport on marker click
 	        marker.addListener('click', function() {
 	            controller.activeMarker = this;
 	            controller.airport = _.findWhere(controller.airports, {Code: this.title});
 	            // update the UI
 	            $scope.$apply();
 	        });
+	        
+	        // remember the code
 	        controller.seletedAirports.push(controller.airport.Code);
         }
+	    
+	    // move the map a bit
         controller.map.panTo(latLng);
         controller.activeMarker = null;
 	};
 	
-   controller.initGoogleMap = function(map) {
+	/**
+	 * Setter for the map
+	 */
+    controller.initGoogleMap = function(map) {
        controller.map = map;
     };
 	
+    /**
+     * Is called on delete button click
+     */
     controller.deleteMarker = function() {
+        // remove marker from map and selected list
         controller.activeMarker.setMap(null);
         controller.seletedAirports = _.without(controller.seletedAirports, controller.airport.Code) ;
         controller.activeMarker = null;
