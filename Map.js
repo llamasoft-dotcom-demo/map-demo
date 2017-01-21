1da2a1008efd44d3ce7248ef39de8e1b4916d885
Andrew Brainerd <drwb333@gmail.com>
2017-01-21 14:03:04 -0500

    Initial Commit. Small starting changes.

--- Map.js
+++ Map.js
@@ -1,65 +1,47 @@
-var globalMap;
-
-$(function() {
-
-var MapFcns = {
-    loadSiteList: function () {
-        var airportList = $('#airport-list');
-            airportList.html('');
-            airportList.append('<option value=""></option>');
-        for (var i in sites) {
-            var newOption = $('<option value="' + sites[i].Code + '">' + sites[i].Code + '</option>');
-            airportList.append(newOption);
-        }
-    },
-    
-    siteListChange: function() {
-        var ctl = $(this),
-            airportCode = ctl.val();
-            if(airportCode) {
+var globalMap, airportList;
+$(function () {
+    var MapFcns = {
+        loadSiteList: function () {
+            airportList = $("#airport-list");
+            airportList.html("<option value='-- Select an Airport --'></option>");
+            for (var i in sites) {
+                var newOption = $("<option value='" + sites[i].Code + "'>" + sites[i].Code + "</option>");
+                airportList.append(newOption);
+            }
+        },
+        siteListChange: function () {
+            var ctl = $(this), airportCode = ctl.val();
+            if (airportCode) {
                 var currAirport = _.findWhere(sites, {Code: airportCode});
-                $('#setting-code').text(currAirport.Code);
-                $('#setting-city').text(currAirport.City);
-                
+                $("setting-code").text(currAirport.Code);
+                $("#setting-city").text(currAirport.City);
                 var marker = new google.maps.Marker({
                     position: {lat: currAirport.Latitude, lng: currAirport.Longitude},
                     map: globalMap,
                     title: currAirport.Code
                 });
             }
-    }
+        }
+    };
+    MapFcns.loadSiteList();
+    airportList.change(MapFcns.siteListChange);
+    $("#exercise-toggle").click(function () {
+        var instructions = $("#exercise-instructions");
+        var toggleCtl = $(this), toggleVal = toggleCtl.text();
+        if (toggleVal == '-') {
+            toggleCtl.text('+');
+            instructions.hide();
+        } else {
+            toggleCtl.text('-');
+            instructions.show();
+        }
+    });
+});
+function initMap() {
+    // Callback function to create a map object and specify the DOM element for display.
+    globalMap = new google.maps.Map(document.getElementById('airport-map'), {
+        center: {lat: 42.2814, lng: -83.7483},
+        scrollwheel: true,
+        zoom: 6
+    });
 }
-
-
-MapFcns.loadSiteList();
-$('#airport-list').change(MapFcns.siteListChange);
-$('#exercise-toggle').click(function() {
-    var  toggleCtl = $(this),
-         toggleVal = toggleCtl.text();
-    if (toggleVal == '-') {
-        toggleCtl.text('+');
-        $('#exercise-instructions').hide();
-    } else {
-        toggleCtl.text('-');
-        $('#exercise-instructions').show();
-    }
-});
-
-});
-
-
-
-
-
-
-
-    
-function  initMap() {
-  // Callback function to create a map object and specify the DOM element for display.
-  globalMap = new google.maps.Map(document.getElementById('airport-map'), {
-    center: {lat: 42.2814, lng: -83.7483},
-    scrollwheel: true,
-    zoom: 6
-  });
-  
-    }
