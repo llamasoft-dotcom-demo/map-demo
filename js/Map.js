aa2c616d89c1ef63c35cf6e515f3a1a2f655e438
Andrew Brainerd <abrainerd@umcu.org>
2017-02-21 17:36:23 -0500

    Added in a live search bar, because using the dropdown was sad

--- js/Map.js
+++ js/Map.js
@@ -1,5 +1,6 @@
 var globalMap,
     airportList,
+    airportList2,
     confirmPopup,
     page,
     currAirport,
@@ -7,18 +8,21 @@ var globalMap,
     instructions,
     removeButton,
     selectedAirport,
-    infoWindow;
+    infoWindow,
+    searchInput,
+    results;
 var markers = {};
 $(function () {
     instructions = $("#exercise-instructions");
     controlButtons = $(".button");
-    removeButton = $("#removeMarkerButton");
+    removeButton = $("#remove-marker");
+    searchInput = $("#airport-search");
     instructions.hide();
     controlButtons.hide();
+
     var MapFcns = {
         loadSiteList: function () {
             airportList = $("#airport-list");
-            airportList.html("<option value='-1'>-- Select an Airport --</option>");
             var ordSites = sortByKey(sites, "Code");
 
             for (var s in ordSites) {
@@ -38,6 +42,7 @@ $(function () {
                 $(".row.name").find("div.value").text(formattedSiteName);
                 $(".row.latitude").find("div.value").text(currAirport.Latitude);
                 $(".row.longitude").find("div.value").text(currAirport.Longitude);
+                $("#airport-header").text(formattedSiteName);
 
                 infoWindow = new google.maps.InfoWindow({
                     content: "<div class='table'>" + $("#airport-settings").html() + "</div>",
@@ -76,6 +81,29 @@ $(function () {
         },
         goToLocation: function () {
             moveMap(currAirport.Latitude, currAirport.Longitude);
+        },
+        siteSearch: function (query) {
+            query = query.toUpperCase();
+            results = $("#search-results");
+            results.empty();
+            var counter = 0;
+            var airports = $("option:contains('" + query + "'), option[value*='" + query +"']");
+            console.log("option:contains('" + query + "'), option[value*='" + query +"']");
+            $.each(airports, function (i, v) {
+                if (counter < 10) {
+                    results.append("<li id='" + airports[i].value + "'>" + airports[i].innerText + "</li>");
+                }
+                counter++;
+            });
+            if (counter == 0) {
+                results.append("<li>No Results Found</li>");
+                return;
+            }
+            results.find("li").click(function () {
+                var searchSelection = $(this).attr("id");
+                results.empty();
+                airportList.val(searchSelection).trigger("change");
+            });
         }
     };
     MapFcns.loadSiteList();
@@ -90,6 +118,16 @@ $(function () {
             toggleCtl.text('-');
             instructions.slideDown();
         }
+    });
+    searchInput.keyup(function () {
+        if ($(this).val() != "")
+            MapFcns.siteSearch($(this).val());
+        else
+            results.empty();
+    });
+    searchInput.focus(function () {
+        if ($(this).val() != "")
+            MapFcns.siteSearch($(this).val());
     });
 });
 function printMarkers() {
@@ -120,3 +158,10 @@ function sortByKey(array, key) {
         return ((x < y) ? -1 : ((x > y) ? 1 : 0));
     });
 }
+
+// Extending jQuery Contains selector to be case-insensitive
+$.expr[":"].contains = $.expr.createPseudo(function(arg) {
+    return function( elem ) {
+        return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+    };
+});
