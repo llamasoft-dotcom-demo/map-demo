2fb17ae25656c0ba4939339a14396a0859777c32
Rebecca Decker <missviolence@Rebeccas-MacBook-Pro.local>
2017-10-29 16:36:37 -0400

    Rebecca - Map Demo

--- Map.js
+++ Map.js
@@ -1,4 +1,5 @@
 var globalMap;
+var markers = [];
 
 $(function() {
 
@@ -7,8 +8,9 @@ var MapFcns = {
         var airportList = $('#airport-list');
             airportList.html('');
             airportList.append('<option value=""></option>');
-        for (var i in sites) {
-            var newOption = $('<option value="' + sites[i].Code + '">' + sites[i].Code + '</option>');
+            var sortedSites = _.sortBy(sites, 'Code');
+        for (var i in sortedSites) {
+            var newOption = $('<option value="' + sortedSites[i].Code + '">' + sortedSites[i].Code + '</option>');
             airportList.append(newOption);
         }
     },
@@ -20,14 +22,47 @@ var MapFcns = {
                 var currAirport = _.findWhere(sites, {Code: airportCode});
                 $('#setting-code').text(currAirport.Code);
                 $('#setting-city').text(currAirport.City);
-                
-                var marker = new google.maps.Marker({
-                    position: {lat: currAirport.Latitude, lng: currAirport.Longitude},
-                    map: globalMap,
-                    title: currAirport.Code
-                });
+                $('#setting-state').text(currAirport.State);
+                $('#setting-fullname').text(currAirport.FullSiteName);
+                $('#setting-lat').text(currAirport.Latitude);
+                $('#setting-long').text(currAirport.Longitude);
+
+                var ExistingMarker = _.findWhere(markers, {title: airportCode});
+
+                if(typeof ExistingMarker === "undefined"){
+                    var marker = new google.maps.Marker({
+                            position: {lat: currAirport.Latitude, lng: currAirport.Longitude},
+                            map: globalMap,
+                            title: currAirport.Code
+                        });
+
+                        markers.push(marker);
+
+                        globalMap.panTo(marker.position);
+                        globalMap.setZoom(6);
+
+                        google.maps.event.addListener(marker, "rightclick", function (point) { id = this.__gm_id; removeMarker(marker) });
+
+                }
+                else {
+                        globalMap.panTo(ExistingMarker.position);
+                        globalMap.setZoom(6);    
+                }   
+
             }
     }
+}
+
+
+var removeMarker = function(marker) {
+    var isDelete = confirm("Delete Marker?");
+
+    if(isDelete == true)
+    {
+         marker.setMap(null);
+         markers.pop(marker);
+    }
+   
 }
 
 
