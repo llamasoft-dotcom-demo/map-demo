cb65e1fafa9a8add3a2246956cca7659c0a472af
jakemager <jmager12@gmail.com>
2017-11-12 15:34:28 -0500

    Implemented programming tasks

--- Map.js
+++ Map.js
@@ -1,59 +1,152 @@
 var globalMap;
+var activeInfoWindow;
+var markers = new Map();
+var infoWindows = new Map();
 
 $(function() {
 
-var MapFcns = {
-    loadSiteList: function () {
-        var airportList = $('#airport-list');
-            airportList.html('');
-            airportList.append('<option value=""></option>');
-        for (var i in sites) {
-            var newOption = $('<option value="' + sites[i].Code + '">' + sites[i].Code + '</option>');
-            airportList.append(newOption);
-        }
-    },
-    
-    siteListChange: function() {
-        var ctl = $(this),
-            airportCode = ctl.val();
-            if(airportCode) {
-                var currAirport = _.findWhere(sites, {Code: airportCode});
-                $('#setting-code').text(currAirport.Code);
-                $('#setting-city').text(currAirport.City);
-                
-                var marker = new google.maps.Marker({
-                    position: {lat: currAirport.Latitude, lng: currAirport.Longitude},
-                    map: globalMap,
-                    title: currAirport.Code
-                });
+    var MapFcns = {
+        loadSiteList: function () {
+
+            // create and load array for the jquery autocomplete form
+            var airportList = new Array();
+            for (var i in sites) {
+                airportList.push(sites[i].Code);
             }
+
+            // assign the src
+            $( "#airportList" ).autocomplete({
+              source: airportList
+            });
+        },
+        
+        siteListChange: function() {
+            var ctl = $(this),
+                airportCode = ctl.val();
+                if(airportCode) {
+                    var currAirport = _.findWhere(sites, {Code: airportCode});
+
+                    // close any active infoWindows
+                    if (activeInfoWindow) { 
+                        activeInfoWindow.close();
+                    }
+
+                    // check if the user already selected this airport, else add it!
+                    if (markers.get(currAirport.Code)) {
+                        activeInfoWindow = infoWindows.get(currAirport.Code);   // just set new infoWindow and continue
+                    } else {
+
+                    // infoWindow body
+                    var contentString = '<div id="content">' +
+                                          '<div id="siteNotice">' +
+                                          '</div>' +
+                                          '<h1 id="firstHeading" class="firstHeading">' + currAirport.City +'</h1>' +
+                                          '<div id="bodyContent">' +
+                                            '<p>Code: ' + currAirport.Code + '</p>' +
+                                            '<p>State: ' + currAirport.State + '</p>' +
+                                            '<p>Full Name: ' + currAirport.FullSiteName + '</p>' +
+                                            '<p>Latitude: ' + currAirport.Latitude + '</p>' +
+                                            '<p>Longitude: ' + currAirport.Longitude + '</p>' +
+                                          '</div>' +
+                                        '</div>';
+
+
+                    // make a new info window for this marker
+                    var infowindow = new google.maps.InfoWindow({
+                        content: contentString
+                    });
+
+                    // make temp info window so it closes without losing any data
+                    activeInfoWindow = new google.maps.InfoWindow({
+                        content: contentString
+                    });
+
+                    // make the marker
+                    var marker = new google.maps.Marker({
+                        position: {lat: currAirport.Latitude, lng: currAirport.Longitude},
+                        map: globalMap,
+                        title: currAirport.Code,
+                    });
+
+                    // make click listener for the marker to open info window and close/replace temp info window
+                    marker.addListener('click', function() {
+                        infowindow.open(globalMap, marker);
+                        if (activeInfoWindow) { 
+                            activeInfoWindow.close();
+                        }
+                        activeInfoWindow = infowindow;
+                    });
+
+                    // go to new marker
+                    globalMap.panTo(marker.position);
+
+                    markers.set(currAirport.Code, marker);
+                    infoWindows.set(currAirport.Code, infowindow);
+
+                    var markerDiv = "<div id='" + currAirport.Code + "'>" +
+                                        "<div class='currentMarkerText' onclick='goToMarker(\"" + currAirport.Code + "\")'>" +
+                                            "<p>" + currAirport.Code + ", " + currAirport.City + "</p>" + 
+                                        "</div>" +
+                                        "<div class='currentMarkerDelete' onclick='deleteMarker(\"" + currAirport.Code + "\")'><i class=' fa'>&#xf014;</i></div>" +
+                                    "</div>";
+                    $('#markersContainer').append(markerDiv);
+                }
+
+                // open temp infowindow either way 
+                activeInfoWindow.open(globalMap, markers.get(currAirport.Code));
+
+            }
+        },
     }
+
+
+    MapFcns.loadSiteList(); // load siteData
+
+    // clear textbox onclick
+    $( "#airportList" ).click(function() {
+        $(this).val('');
+    });
+
+    $('#airportList').change(MapFcns.siteListChange);
+    $('#airportList').autocomplete(MapFcns.siteListChange);
+
+    $('#exercise-toggle').click(function() {
+        var  toggleCtl = $(this),
+             toggleVal = toggleCtl.text();
+        if (toggleVal == '-') {
+            toggleCtl.text('+');
+            $('#exercise-instructions').hide();
+        } else {
+            toggleCtl.text('-');
+            $('#exercise-instructions').show();
+        }
+    });
+
+});
+
+
+// when marker is clicked, move map to marker
+function goToMarker(code) {
+
+    globalMap.panTo(markers.get(code).position);
+
+    // close any infoWindows that are currently open and open new infoWindow
+    if (activeInfoWindow) { 
+        activeInfoWindow.close();
+    }
+
+    activeInfoWindow = infoWindows.get(code);
+    activeInfoWindow.open(globalMap, markers.get(code));
 }
 
+// remove pin off map and delete the marker from markers, infoWindows, and markerText
+function deleteMarker(code) {
+    markers.get(code).setMap(null);
+    markers.delete(code);
+    infoWindows.delete(code);
+    $('#' + code).remove();
+}
 
-MapFcns.loadSiteList();
-$('#airport-list').change(MapFcns.siteListChange);
-$('#exercise-toggle').click(function() {
-    var  toggleCtl = $(this),
-         toggleVal = toggleCtl.text();
-    if (toggleVal == '-') {
-        toggleCtl.text('+');
-        $('#exercise-instructions').hide();
-    } else {
-        toggleCtl.text('-');
-        $('#exercise-instructions').show();
-    }
-});
-
-});
-
-
-
-
-
-
-
-    
 function  initMap() {
   // Callback function to create a map object and specify the DOM element for display.
   globalMap = new google.maps.Map(document.getElementById('airport-map'), {
@@ -62,4 +155,4 @@ function  initMap() {
     zoom: 6
   });
   
-    }
+}
